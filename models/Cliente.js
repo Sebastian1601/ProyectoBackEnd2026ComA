1c1ac392d93d686ee554d02f64f820fa7ffd8dc3
Ignacio-Humphreys <humphreysignacio@gmail.com>
2026-09-04 21:23:25 -0300

    Se crean los atributos faltantes de las clases

---
+++ models/Cliente.js
@@ -0,0 +1,8 @@
+class Cliente {
+    constructor(id, nombre, telefono, direccion) { 
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+}
