be1eacd8312b795358107be8cd5c1bf347b10f4e
Cristel Frias <cristel.frias@bue.edu.ar>
2026-09-15 00:56:41 -0300

    Configura Pug como motor de plantillas

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const { router } = require("./routes/routerMain.js");
-const { ValidarString } = require("./validation/clientes.js");
 const { manejadorDeErrores } = require("./ErrorControl/Errores.js");
 
-
 const app = express();
+
+// Configuración de Pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// Middleware para recibir JSON
 app.use(express.json());
-app.use('/', router);
+
+// Página principal realizada con Pug
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Rutas de la API
+app.use("/", router);
+
+// Manejo de errores
 app.use(manejadorDeErrores);
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
 });
